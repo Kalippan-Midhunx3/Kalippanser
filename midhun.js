3cf8085a28efa898cd4048087f62ed850e6c7f08
Kalippan-Midhunx3 <101493549+Kalippan-Midhunx3@users.noreply.github.com>
2022-03-13 20:51:52 +0530

    Update midhun.js

--- midhun.js
+++ midhun.js
@@ -12,6 +12,6 @@ function convertToBool(text, fault = 'true') {
 
 module.exports = {
  PLKAFN: process.env.BAD_WORDS === undefined ? false : process.env.BAD_WORDS,
- PHONE: process.env.NUMBER === undefined ? '+917025631103' : process.env.NUMBER,   
- OA_NAME: process.env.DEPLOYER === undefined ? 'Amru-ser' : process.env.DEPLOYER,    
+ PHONE: process.env.NUMBER === undefined ? '+918281370025' : process.env.NUMBER,   
+ OA_NAME: process.env.DEPLOYER === undefined ? 'Kalippan-ser' : process.env.DEPLOYER,    
 };
