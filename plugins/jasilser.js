8bf75751ea380f485a3a0ac65c858824cc1adef2
Kalippan-Midhunx3 <101493549+Kalippan-Midhunx3@users.noreply.github.com>
2022-03-14 14:50:36 +0530

    Update jasilser.js

--- plugins/jasilser.js
+++ plugins/jasilser.js
@@ -2,7 +2,7 @@
 Licensed under the  GPL-3.0 License;
 you may not use this file except in compliance with the License.
 Amalser - Amal
-Wa.me/+918281440156
+Wa.me/+918281370025
 */
 
 const Neotro = require('../events');
@@ -20,18 +20,18 @@ var ddd = ggg.toString('utf-8')
 Neotro.addCommand({pattern: 'help', fromMe: false, dontAddCommandList: true}, (async (message, match) => {
 // send a list message!
     const rows = [
-        {title: 'Aʙᴜ Bᴏᴛ Oᴡɴᴇʀs Nᴜᴍʙᴇʀ', description: "⚙Bᴏᴛ Oᴡɴᴇʀ Nᴜᴍᴇʀ\n\n\n*◁ === Tɪs Oᴡɴᴇʀs Aʙᴜ Sᴇʀ Nᴀɴʙᴜɴ Aɴᴅ Cʜᴜɴᴋs === ▷*\n\n🎭 *Aʙᴜ Bᴏᴛ Bʏ Aʙᴜ sᴇʀ *\n 🔮 http://wa.me/+917025994178.\n\n🎭 *Aʙᴜ Bᴏᴛ Bʏ Lᴜᴄᴋʏ Sᴇʀ*\n🔮 http://wa.me/+916238748280\n\n🎭 *Aʙᴜ Bᴏᴛ Bʏ Kᴀʟɴ*\n🔮 http://wa.me/+916238532742\n\n🎭 *Aʙᴜ Bᴏᴛ Bʏ Aᴘᴘᴜ Sᴇʀ*\n🔮 http://wa.me/+918590432917\n\n ", rowId:" rowid1"},
+        {title: 'Kᴀʟɪᴘᴘᴀɴ sᴇʀ Oᴡɴᴇʀs Nᴜᴍʙᴇʀ', description: "⚙Bᴏᴛ Oᴡɴᴇʀ Nᴜᴍᴇʀ\n\n\n*◁ === Tɪs Oᴡɴᴇʀs Kᴀʟɪᴘᴘᴀɴ Nᴀɴʙᴜɴᴢ Aɴᴅ Cʜᴜɴᴋs === ▷*\n\n🎭 *Kᴀʟɪᴘᴘᴀɴ sᴇʀ Bʏ Kᴀʟɪᴘᴘᴀɴ sᴇʀ *\n 🔮 http://wa.me/+918281370025.\n\n🎭 *Kᴀʟɪᴘᴘᴀɴ sᴇʀ Bʏ Oɢɢʏ sᴇʀ*\n🔮 http://wa.me/+919400048101\n\n🎭 *Kᴀʟɪᴘᴘᴀɴ sᴇʀ Bʏ Tʀᴏxɪɴ sᴇʀ*\n🔮 http://wa.me+919633049651\n\n🎭 *Kᴀʟɪᴘᴘᴀɴ sᴇʀ Bʏ Aᴘᴘᴜ Sᴇʀ*\n🔮 http://wa.me/+918590432917\n\n ", rowId:" rowid1"},
         {title: 'Gɪᴛ', description: "Aʙᴜ Sᴇʀ Bᴏᴛ Mᴀᴋɪɴɢ Lɪɴᴋ.\n\n\n*◁ === 🥰🤗 Gɪᴛ Lɪɴᴋ === ▷*\n\nhttps://github.com/Arx-Abu/Abu_ser\n\n ", rowId:"rowid2"},
-        {title: 'Aʙᴜ Bᴏᴛ Oғғɪᴄᴀʟ Gʀᴏᴜᴘ Bʏ Kᴀʟᴀɴ', description: "Gʀᴏᴜᴘ Lɪɴᴋ .\n\n\nhttps://chat.whatsapp.com/Bq0eHs3UpGJ2BKIHOmy7mk\n\n ", rowId:" rowid5"},
-        {title: 'Kᴀʟɴ Sᴇʀ Cʜᴀɴᴀʟ Lɪɴᴋ ', description: "Aʙᴜ Bᴏᴛ Mᴀᴋɪɴɢ Vɪᴅᴇᴏ Tʜɪs Cʜᴀɴᴀʟ\n\n\n*Kᴀʟᴀɴ Sᴇʀ Cʜᴀɴᴀʟ Sᴜʙsᴄʀᴀɪʙᴇ*\n\n*https://youtu.be/OYhA_ZNQ4GQ* ", rowId:" rowid6"},
+        {title: 'Kᴀʟɪᴘᴘᴀɴ sᴇʀ Oғғɪᴄᴀʟ Gʀᴏᴜᴘ Bʏ Oɢɢʏ, description: "Gʀᴏᴜᴘ Lɪɴᴋ .\n\n\nhttps://chat.whatsapp.com/CDNbORWtY1KCPyrm5qNyIl\n\n ", rowId:" rowid5"},
+        {title: 'Oɢɢʏ Sᴇʀ Cʜᴀɴᴀʟ Lɪɴᴋ ', description: "Cʜᴀɴᴀʟ\n\n\n*Kᴀʟᴀɴ Sᴇʀ Cʜᴀɴᴀʟ Sᴜʙsᴄʀᴀɪʙᴇ*\n\n*https://youtu.be/tzq89jsNOmE* ", rowId:" rowid6"},
         {title: 'Bᴏᴛ Gʀᴏᴜᴘ Dᴇsᴄʀɪᴘᴛɪᴏɴ', description: "Nɪʏᴀᴍᴀᴍ Iɴɪ Eʟʟᴀᴠᴀʀᴋᴋᴜᴍ Bᴀᴅʜᴀᴋᴀ Mᴀɴ \n\n\n☃️ᴏɴɴᴜᴍ ɪʟʟᴀ ɢʀᴏᴜᴘ ᴅᴇsᴄʀɪᴘᴛɪᴏɴ😁 \n\n  ", rowId:" rowid7"}
        ]
        
-       const sections = [{title: "Aʙᴜ sᴇʀ ❤️", rows: rows}]
+       const sections = [{title: "Kᴀʟɪᴘᴘᴀɴ sᴇʀ ❤️", rows: rows}]
        
        const button = {
-        buttonText: '💚Aʙᴜ Bᴏᴛ💚',
-        description: "💖✻Aʙᴜ Sᴇʀ Bᴏᴛ✻💖",
+        buttonText: '💚Kᴀʟɪᴘᴘᴀɴ sᴇʀ💚',
+        description: "💖✻Kᴀʟɪᴘᴘᴀɴ sᴇʀ Bᴏᴛ✻💖",
         sections: sections,
         listType: 1
        }
