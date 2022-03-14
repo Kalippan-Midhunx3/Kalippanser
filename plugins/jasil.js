2e9bcc5490a9fd8409cf6bbc22f684cc45a34dbf
Kalippan-Midhunx3 <101493549+Kalippan-Midhunx3@users.noreply.github.com>
2022-03-14 12:08:58 +0530

    Update jasil.js

--- plugins/jasil.js
+++ plugins/jasil.js
@@ -7,22 +7,22 @@ const Lang = Language.getString('wallpaper');
 
 Asena.addCommand({pattern: 'git', fromMe: false, desc: "Gives github link of the bot"}, (async (message, match) => {
 
-    var skl = await axios.get("https://i.imgur.com/CtaexU3.jpeg", { responseType: 'arraybuffer' })
+    var skl = await axios.get("https://www.linkpicture.com/q/20220313_142355.jpg", { responseType: 'arraybuffer' })
 
-    await message.sendMessage(Buffer(skl.data), MessageType.image, {mimetype: Mimetype.png, caption: `*Bᴏᴛ Nᴀᴍᴇ 🇦 🇲 🇷 🇺   🇸 🇪 🇷  🇧 🇴 🇹 *
+    await message.sendMessage(Buffer(skl.data), MessageType.image, {mimetype: Mimetype.png, caption: `*Bᴏᴛ Nᴀᴍᴇ Ⓚ︎Ⓐ︎Ⓛ︎Ⓘ︎Ⓟ︎Ⓟ︎Ⓐ︎Ⓝ︎ Ⓢ︎Ⓔ︎Ⓡ︎*
 
-*Cʀᴇᴀᴛᴇʀ number : wa.me/917025631103?text=Hi%20ᴀᴍʀᴜ%20bro.%20*
+*Cʀᴇᴀᴛᴇʀ number : wa.me/918281370025?text=Hi%20Kᴀʟɪᴘᴘᴀɴ%20bro.%20*
 
 
- *Aᴍʀᴜ sᴇʀ Cʜᴀɴᴀʟ : https://youtu.be/x47g62HcsWc😁*
+ *Aᴍʀᴜ sᴇʀ Cʜᴀɴᴀʟ : https://youtube.com/channel/UC0yNrBziB3u2hzvXzJ4NnTA💖*
      
- *Iɴsᴛᴀɢʀᴀᴍ ɪᴅ: https://instagram.com/its_me_dron?utm_medium=copy_link*
+ *Iɴsᴛᴀɢʀᴀᴍ ɪᴅ: https://instagram.com/_midhun_x3__medium=copy_link*
 
- *Aᴍʀᴜ sᴇʀ Bᴏᴛ Gʀᴏᴜᴘ: https://chat.whatsapp.com/DnHJu25Ccss7zn72nPhL8z*
+ *Kᴀʟɪᴘᴘᴀɴ sᴇʀ Bᴏᴛ Gʀᴏᴜᴘ: https://chat.whatsapp.com/CDNbORWtY1KCPyrm5qNyIl*
 
- *Gɪᴛ Lɪɴᴋ : https://github.com/AMRUSIR/AMRU-SER*
+ *Gɪᴛ Lɪɴᴋ : https://github.com/Kalippan-Midhunx3/Kalippanser*
 
- *Aᴍʀᴜ-sᴇʀ-ʙᴏᴛꫂ⁩..♡︎*
+ *Kᴀʟɪᴘᴘᴀɴ-sᴇʀ-ʙᴏᴛꫂ⁩..♡︎*
 `}) 
 
 }));
